2090919cfeac4d55ea912f837cc829253a45165a
eximus <fabio_r11@hotmail.com>
2020-09-11 15:25:35 +0100

    extract points script update

--- scripts/extractPoint/extractPoint.py
+++ scripts/extractPoint/extractPoint.py
@@ -1,9 +1,38 @@
 #!/home/rmc/progs/python/anaconda3/bin/python3
 
 import netCDF4 as nc
+import numpy as np
+from geopy import distance
+from datetime import datetime, timedelta
 import time
+import os
+
+from threading import Thread
+import queue
+
 
 URL_ENDPOINT = "http://193.136.153.163/thredds/dodsC/WAVERYS_collection/WAVERYS_Collection_fmrc.ncd"
+RADIUS = 40 # radius in km
+CHUNK_SIZE = 60 # size of each request
+OUTPUT_DIR = 'out/'
+
+def threaded(function, daemon=False):
+	'''Decorator to make a function threaded
+
+	To acess wrapped function return value use .get() on the return value
+	'''
+	def wrapped_function(queue, *args, **kwargs):
+		return_val = function(*args, **kwargs)
+		queue.put(return_val) 
+	def wrap(*args, **kwargs):
+		queue = queue.Queue()
+
+		thread = Thread(target=wrapped_function, args=(queue,)+args , kwargs=kwargs)
+		thread.daemon=daemon
+		thread.start()
+		thread.result_queue=queue
+		return thread
+	return wrap
 
 def load_points(filename):
 	'''Load points from file with the line format:
@@ -16,7 +45,7 @@ def load_points(filename):
 	with open(filename, 'r') as fp:
 		return [make_tuple(line.strip('\n').split()) for line in fp]
 
-def query_thredds_opendap(endpoint=URL_ENDPOINT, **var_query):
+def query_thredds_opendap(endpoint=URL_ENDPOINT, verbose=False, **var_query):
 	'''Query the opendap endpoint for a certain variable with given parameters:
 	The variable must be a named function argument and the parameters must be a tuple or list of tuples
 	Example:
@@ -45,8 +74,61 @@ def query_thredds_opendap(endpoint=URL_ENDPOINT, **var_query):
 				yield f"{var}{process_list(value)}"
 
 	url = endpoint + '?' + ','.join(process_args())
+	if verbose: print(url)
 	return nc.Dataset(url)
 
+def get_nearest_points(points, point, radius):
+	'''Returns a list of indexes in points that are inside a radius of point'''
+	points = np.asarray(points)
+	idx = (np.abs(points - point)).argmin() # TODO Check for speed if done too many times...
+	indexes = [idx]
+	for i in range(len(points)):
+		# TODO this is not the ideal way of calculating distances
+		if idx+i+1 < len(points) and abs(points[idx]-points[idx+i+1]) <radius/110:
+			indexes.append(idx+i+1)
+		if idx-i-1 > 0 and abs(points[idx]-points[idx-i-1]) < radius/110:
+			indexes.insert(0, idx-i-1)
+	return indexes
+
+def wf(pdist):
+	a=(1 - pdist / 40.0)
+	return (abs(a)+a)/2
+
+def nanaverage(A,weights):
+	return np.nansum(A*weights)/((~np.isnan(A))*weights).sum()
+
+def process_data(data, point, variable, weight_function=wf):
+	'''Process data using the weighted average of each data point around the point given'''
+
+	# Calculate weights for each point around the target
+	weights = np.zeros((len(data.variables[LATITUDE]), len(data.variables[LONGITUDE])))
+	for i in range(len(data.variables[LATITUDE])):
+		for j in range(len(data.variables[LONGITUDE])):
+			dist = distance.distance((point[0],point[1]), (data.variables[LATITUDE][i], data.variables[LONGITUDE][j])).km
+			weights[i,j] = weight_function(dist)
+
+	basedate = datetime.strptime(data.variables['time'].units.split()[-1], "%Y-%m-%dT%H:%M:%SZ")
+	timestep = data.variables['time'].units.split()[0] # should be "hours" in most cases
+	times = data.variables['time'][:]
+	size = len(times)
+
+	# Write Header
+	with open(f"{OUTPUT_DIR}{point[-1]}.txt", 'w') as fp:
+		fp.write(f"TIME{' '*(15)}   {variable}\n")
+
+	# makes requests in sizes of CHUNK_SIZE. ex if CHUNK_SIZE=20 and size=47 -> [0,20] [20,40] [40,47]
+	for mi, ma in ((i,i+(CHUNK_SIZE if size-i>CHUNK_SIZE else size-i)) for i in range(0,size,CHUNK_SIZE)):
+		t = time.time()
+		tmp_data = data.variables[variable][mi:ma]
+		print('Request:',  time.time() - t)
+
+		with open(f"{OUTPUT_DIR}{point[-1]}.txt", 'a') as fp:
+			for i in range(len(tmp_data)):
+				for j in range(d.variables['time'].shape[1]):
+					x = nanaverage(tmp_data[i][j], weights)
+					tx = basedate+timedelta(**{timestep:times[mi+i][j]})
+					fp.write(f"{str(tx)}   {str(x)}\n")
+	
 
 # ==============================================
 #                   MAIN
@@ -67,10 +149,19 @@ lon_size = data.variables[LONGITUDE].size
 # LOAD COORDINATES
 coordinates = query_thredds_opendap(URL_ENDPOINT, longitude=(0,lon_size-1), latitude=(0,lat_size-1))
 
+# CREATE OUTPUT DIR
+if not os.path.exists(OUTPUT_DIR):
+	os.makedirs(OUTPUT_DIR)
+
 # QUERY DATA NEXT TO POINTS
 for p in points:
+	lat_range = get_nearest_points(coordinates.variables[LATITUDE], p[0], RADIUS)
+	lon_range = get_nearest_points(coordinates.variables[LONGITUDE], p[1], RADIUS)
 
-	break
+	d = query_thredds_opendap(URL_ENDPOINT, VHM0=[(0,9824), (0,7), (lat_range[0], lat_range[-1]), (lon_range[0], lon_range[-1])],
+											time=[(0,9824), (0,7)],
+											latitude=(lat_range[0], lat_range[-1]),
+											longitude=(lon_range[0], lon_range[-1]))
 
-# "http://193.136.153.163/thredds/dodsC/WAVERYS_collection/WAVERYS_Collection_fmrc.ncd?VHM0[0:1:0][0:1:1][0:1:0][0:1:0]"
-# "http://193.136.153.163/thredds/dodsC/WAVERYS_collection/WAVERYS_Collection_fmrc.ncd?longitude[0:1799],latitude[0:898]"
+	process_data(d, p, 'VHM0')
+

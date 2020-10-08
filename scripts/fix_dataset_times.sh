333d32c1d451fe47bee6d9299e547300216cf3c6
eximus <fabio_r11@hotmail.com>
2020-10-08 04:33:01 +0100

    added script to cut and process ww3, but cdo is ridiculous expanding file size...

--- scripts/fix_dataset_times.sh
+++ scripts/fix_dataset_times.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # This script is intended to use only on CFSR and CFSv2 hindcast data.
-#  The goal is to move out of place data to other files (timestamps that belong to different files) 
+#  The goal is to move out of place data to other files (timestamps that belong to different files)
 #
 # This scripts expects a list of files as arguments from which to move timestamps between.
-# 
+#
 # Behavior:
 # The files are sorted alphabeticaly and for each pair it is verified
 #  - IF TIME OVERLAP OCURRS. In which case the last time from the first file
 #    of the overlapping pair is removed
 #  - IF ENDING TIME BELONGS TO NEXT FILE. In which case the last time from the
 #    first file is removed and stored in the next file
-# 
+#
 #  The reverse is also verified
 #  - IF BEGINING OF NEXT FILE BELONGS TO THE PREVIOUS FILE.	In which case it is copied and removed from the original
 
@@ -51,12 +51,12 @@ ti=time.gmtime(int(d.variables['time'][0]));tf=time.gmtime(int(d.variables['time
 print(f'{ti.tm_year}{ti.tm_mon:02}' == f'{tf.tm_year}{tf.tm_mon:02}' == sys.argv[1].split('.')[-2] and ti.tm_hour == 0 and tf.tm_hour == 23)" "$1"
 }
 
-# Sort files given
 if [[ $# = 0 ]]; then
 	echo "Give the files to be processed as arguments to the script."
 	exit
 fi
 
+# Sort files given
 files=( $(sort <(printf "%s\n" "$@")) )
 mkdir -p $TEMP
 
@@ -66,45 +66,45 @@ for (( i=1 ; i<${#files[@]} ; i++ )); do
 	fi
 	echo Processing $i/$((${#files[@]}-1)) -- ${files[$i-1]}   ${files[$i]}
 
-	
+
 	# CHECK HOW MANY DATE POINTS TO EXTRACT FROM FIRST FILE
 	npoints=$(check_forward_misplaced_times ${files[$i-1]} ${files[$i]})
 	if [[ $npoints != 0 ]]; then
 		echo -e "  \033[33m==\033[m Times out of place. Moving $npoints timesteps out of ${files[$i-1]}..."
 		# Move selected times to a new file
-		ncks -O -d time,-$npoints,-1 ${files[$i-1]} "$TEMP/$(basename ${files[$i-1]}).tmp" 
+		/usr/bin/ncks -O -d time,-$npoints,-1 ${files[$i-1]} "$TEMP/$(basename ${files[$i-1]}).tmp"
 		comp=$(check_start_end_times_overlap "$TEMP/$(basename ${files[$i-1]}).tmp" ${files[$i]})
 
 		if [[ $npoints != 1 ]] || [[ ! $comp =~ "True" ]]; then
 			if [[ $npoints != 1 ]] && [[ $comp =~ "True" ]]; then
 				# Cut last time of file
 				echo -e "  \033[33m==\033[m Times match ${comp##True }. Deleting on ${files[$i-1]}..."
-				ncks -O -d time,0,-2 "$TEMP/$(basename ${files[$i-1]}).tmp" "$TEMP/$(basename ${files[$i-1]}).no_overlap.tmp" # delete and save a tmp file
+				/usr/bin/ncks -O -d time,0,-2 "$TEMP/$(basename ${files[$i-1]}).tmp" "$TEMP/$(basename ${files[$i-1]}).no_overlap.tmp" # delete and save a tmp file
 				mv "$TEMP/$(basename ${files[$i-1]}).no_overlap.tmp" "$TEMP/$(basename ${files[$i-1]}).tmp" # overwrite original
 			fi
 
 			# Stich tmp file to second file
 			echo -e "  \033[33m==\033[m Stiching extracted timesteps into ${files[$i]}..."
-			ncrcat -O "$TEMP/$(basename ${files[$i-1]}).tmp" ${files[$i]} "$TEMP/$(basename ${files[$i]}).tmp" 
+			/usr/bin/ncrcat -O "$TEMP/$(basename ${files[$i-1]}).tmp" ${files[$i]} "$TEMP/$(basename ${files[$i]}).tmp"
 			mv "$TEMP/$(basename ${files[$i]}).tmp" ${files[$i]}  # ovewrite second file
 		fi
 
 		# Remove the extracted times from the first file
 		echo -e "  \033[33m==\033[m Deleting $npoints timesteps on ${files[$i-1]}..."
-		ncks -O -d time,0,-$(($npoints+1)) "${files[$i-1]}" "$TEMP/$(basename ${files[$i-1]}).tmp" # delete times moved and save tmp file
+		/usr/bin/ncks -O -d time,0,-$(($npoints+1)) "${files[$i-1]}" "$TEMP/$(basename ${files[$i-1]}).tmp" # delete times moved and save tmp file
 		mv "$TEMP/$(basename ${files[$i-1]}).tmp" "${files[$i-1]}" # overwrite first file
 	fi
-	
+
 	# CHECK HOW MANY DATAPOINTS TO EXTRACT FROM THE SECOND FILE (basically the same but in reverse order)
 	npoints=$(check_backward_misplaced_times ${files[$i-1]} ${files[$i]})
 	if [[ $npoints != 0 ]]; then
 		echo -e "  \033[33m==\033[m Times out of place. Moving $npoints timesteps out of ${files[$i]} into ${files[$i-1]}..."
 		# Move selected times to a new file
-		ncks -O -d time,0,$(($npoints-1)) ${files[$i]} "$TEMP/$(basename ${files[$i]}).tmp" 
-		ncrcat -O ${files[$i-1]} "$TEMP/$(basename ${files[$i]}).tmp" "$TEMP/$(basename ${files[$i-1]}).tmp" 
+		/usr/bin/ncks -O -d time,0,$(($npoints-1)) ${files[$i]} "$TEMP/$(basename ${files[$i]}).tmp"
+		/usr/bin/ncrcat -O ${files[$i-1]} "$TEMP/$(basename ${files[$i]}).tmp" "$TEMP/$(basename ${files[$i-1]}).tmp"
 		# Remove selected times from the file
 		echo -e "  \033[33m==\033[m Removing $npoints timesteps out of ${files[$i]}..."
-		ncks -O -d time,$npoints,-1 ${files[$i]} "$TEMP/$(basename ${files[$i]}).tmp" 
+		/usr/bin/ncks -O -d time,$npoints,-1 ${files[$i]} "$TEMP/$(basename ${files[$i]}).tmp"
 		# Overwrite existing files
 		mv "$TEMP/$(basename ${files[$i-1]}).tmp" ${files[$i-1]}
 		mv "$TEMP/$(basename ${files[$i]}).tmp" ${files[$i]}
--- scripts/rename_era5.sh
+++ scripts/rename_era5.sh
@@ -14,7 +14,7 @@ process() {
 	files=`find $DATA -name "$1*.nc" -not -path "*testes*" | sort`
 	total=`find $DATA -name "$1*.nc" -not -path "*testes*" | wc -l`
 	n=1
-	for f in $files; do 
+	for f in $files; do
 		echo ">> [$n/$total] $f ${f/${1}/${2}}"
 		ncrename -v $1,$2 "$f"
 		mv "$f" "${f/${1}/${2}}"

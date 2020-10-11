e5fc00532fa90ce521d8faf96d9f6ec02f2ebc5d
eximus <fabio_r11@hotmail.com>
2020-10-12 00:52:17 +0100

    added overwrite operation for ww3 processing

--- scripts/process_ww3.py
+++ scripts/process_ww3.py
@@ -1,6 +1,6 @@
 #!/home/rmc/progs/python/anaconda3/bin/python3 -u
 
-import sys
+import sys, os
 import xarray
 from dask.diagnostics import ProgressBar
 import numpy as np
@@ -21,11 +21,17 @@ def process_ww3(orig, target):
 	mask_lat = (xdata.latitude >= LATMIN) & (xdata.latitude <= LATMAX)
 	mask_lon = (xdata.longitude >= LONMIN ) & ( xdata.longitude <= LONMAX)
 	xcrop = xdata.where(mask_lat & mask_lon, drop = True) # since chunks was used, this will only be proccessed when writing to disk
+	
+	# for p in np.array(xcrop.partition):
+	# 	xpart = xcrop.sel(partition=p) # select partition 
+	# 	np.where((xpart.significant_wave_height.notnull()) & (xpart.significant_wave_height > 0)) # this will get all the valid values
+	# 	# TODO
+
 
 	# generate encoding for data vars
-	comp = dict(zlib=True, complevel=7) # the original file uses 9 but since we are reducing its size ill use less compression here
+	enc = dict(zlib=True, complevel=7) # the original file uses 9 but since we are reducing its size ill use less compression here
 	with ProgressBar():
-		xcrop.to_netcdf(target, format='NETCDF4_CLASSIC', encoding={var: comp for var in xcrop.data_vars})
+		xcrop.to_netcdf(target, format='NETCDF4_CLASSIC', encoding={var: enc for var in xcrop.data_vars})
 
 
 # ============================
@@ -38,4 +44,6 @@ for file in sys.argv[1:]:
 	stime = time.time()
 	process_ww3(file, file + '.tmp')
 	print("Processed in %.3f" % (time.time() - stime))
+	print(f"Moving {file}")
+	os.rename(file + '.tmp', file) # Overwrite original file
 

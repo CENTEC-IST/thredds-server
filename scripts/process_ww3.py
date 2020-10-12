a486c985aff22351cec51f23584837fbbd956056
eximus <fabio_r11@hotmail.com>
2020-10-12 19:21:32 +0100

    updated script to process ww3

--- scripts/process_ww3.py
+++ scripts/process_ww3.py
@@ -11,27 +11,42 @@ LATMAX=90.
 LONMIN=-102.
 LONMAX=30.
 
-def process_ww3(orig, target):
-	xdata = xarray.open_dataset(orig, chunks={'date':30, 'partition':4}) # process dataset in chunks (to avoid overflowing memory
-	xdata = xdata.rename({'date':'time'}) # rename 
+COMPRESSION = 7
+REMOVE_PARTITIONS_HIGHER = 20 # Remove partition >= 20 , the file with least amount of partitions has 19
 
-	xdata['longitude'] = xdata.longitude - 180 # adjust longitude, latitude should be fine -90, 90
+def process_ww3(orig, target):
+	xdata = xarray.open_dataset(orig)
+
+	if 'date' in xdata: # rename
+		xdata = xdata.rename({'date':'time'})
+	
+	# process dataset in chunks (to avoid overflowing memory)
+	xdata = xdata.chunk({'time':100, 'partition':10})
+	# xdata = xdata.chunk({dim:'auto' for dim in xdata.dims})
+	# xdata = xdata.unify_chunks()
+
+	if xdata.longitude[0] == 0:
+		# adjust longitude, latitude should be fine -90, 90
+		xdata['longitude'] = xdata.longitude - 180 
 
 	# crop
 	mask_lat = (xdata.latitude >= LATMIN) & (xdata.latitude <= LATMAX)
 	mask_lon = (xdata.longitude >= LONMIN ) & ( xdata.longitude <= LONMAX)
-	xcrop = xdata.where(mask_lat & mask_lon, drop = True) # since chunks was used, this will only be proccessed when writing to disk
-	
-	# for p in np.array(xcrop.partition):
-	# 	xpart = xcrop.sel(partition=p) # select partition 
-	# 	np.where((xpart.significant_wave_height.notnull()) & (xpart.significant_wave_height > 0)) # this will get all the valid values
-	# 	# TODO
+	mask_part = (xdata.partition <= REMOVE_PARTITIONS_HIGHER)
+	# since chunks was used, this will only be proccessed when writing to disk
+	xcrop = xdata.where(mask_lat & mask_lon & mask_part, drop = True)
 
+	# for p in np.array(xcrop.partition):
+	# 	xpart = xcrop.sel(partition=p) # select partition
+	# 	np.where((xpart.significant_wave_height.notnull()) & (xpart.significant_wave_height > 0)) # this will get all the valid values
 
 	# generate encoding for data vars
-	enc = dict(zlib=True, complevel=7) # the original file uses 9 but since we are reducing its size ill use less compression here
+	# the original file uses 9 but since we are reducing its size ill use less compression here
+	enc = dict(zlib=True, complevel=COMPRESSION) 
 	with ProgressBar():
 		xcrop.to_netcdf(target, format='NETCDF4_CLASSIC', encoding={var: enc for var in xcrop.data_vars})
+
+	xdata.close()
 
 
 # ============================

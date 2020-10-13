736d0abc302964ee5060e7b7ad9a831cd306b38e
eximus <fabio_r11@hotmail.com>
2020-10-13 14:53:19 +0100

    improved process_ww3, see #15

---
+++ scripts/daemons/alert_disk_space.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# This scripts sends an alert if space in disk gets too low
+
+set -e
+
+# TODO Use mutt to send mail 
--- scripts/process_ww3.py
+++ scripts/process_ww3.py
@@ -11,8 +11,9 @@ LATMAX=90.
 LONMIN=-102.
 LONMAX=30.
 
+# the original file uses 9 but since we are reducing its size ill use less compression here
 COMPRESSION = 7
-REMOVE_PARTITIONS_HIGHER = 20 # Remove partition >= 20 , the file with least amount of partitions has 19
+PARTITIONS_TO_KEEP = 19 # the file with least amount of partitions has 19
 
 def process_ww3(orig, target):
 	xdata = xarray.open_dataset(orig)
@@ -21,7 +22,7 @@ def process_ww3(orig, target):
 		xdata = xdata.rename({'date':'time'})
 	
 	# process dataset in chunks (to avoid overflowing memory)
-	xdata = xdata.chunk({'time':100, 'partition':10})
+	# xdata = xdata.chunk({'time':100, 'partition':10})
 	# xdata = xdata.chunk({dim:'auto' for dim in xdata.dims})
 	# xdata = xdata.unify_chunks()
 
@@ -29,33 +30,31 @@ def process_ww3(orig, target):
 		# adjust longitude, latitude should be fine -90, 90
 		xdata['longitude'] = xdata.longitude - 180 
 
-	# crop
-	mask_lat = (xdata.latitude >= LATMIN) & (xdata.latitude <= LATMAX)
-	mask_lon = (xdata.longitude >= LONMIN ) & ( xdata.longitude <= LONMAX)
-	mask_part = (xdata.partition <= REMOVE_PARTITIONS_HIGHER)
-	# since chunks was used, this will only be proccessed when writing to disk
-	xcrop = xdata.where(mask_lat & mask_lon & mask_part, drop = True)
+	xcrop = xdata.sel(latitude = slice(LATMIN, LATMAX),
+					longitude = slice(LONMIN, LONMAX),
+					partition = slice(0, PARTITIONS_TO_KEEP))
 
-	# for p in np.array(xcrop.partition):
-	# 	xpart = xcrop.sel(partition=p) # select partition
-	# 	np.where((xpart.significant_wave_height.notnull()) & (xpart.significant_wave_height > 0)) # this will get all the valid values
+	# # crop
+	# mask_lat = (xdata.latitude >= LATMIN) & (xdata.latitude <= LATMAX)
+	# mask_lon = (xdata.longitude >= LONMIN ) & ( xdata.longitude <= LONMAX)
+	# mask_part = (xdata.partition < REMOVE_PARTITIONS_HIGHER)
+	# # since chunks was used, this will only be proccessed when writing to disk
+	# xcrop = xdata.where(mask_lat & mask_lon & mask_part, drop = True)
 
 	# generate encoding for data vars
-	# the original file uses 9 but since we are reducing its size ill use less compression here
 	enc = dict(zlib=True, complevel=COMPRESSION) 
 	with ProgressBar():
 		xcrop.to_netcdf(target, format='NETCDF4_CLASSIC', encoding={var: enc for var in xcrop.data_vars})
 
 	xdata.close()
 
-
 # ============================
 #			MAIN
 # ============================
 
-
-for file in sys.argv[1:]:
-	print(f"Processing {file}")
+files = sys.argv[1:]
+for i, file in enumerate(files):
+	print(f"Processing [{i}/{len(sys.argv)-2}] {file}")
 	stime = time.time()
 	process_ww3(file, file + '.tmp')
 	print("Processed in %.3f" % (time.time() - stime))
